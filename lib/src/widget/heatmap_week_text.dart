2d0a364f21ba521480418010f4702b7d35d3510f
voker <xinyd1992@gmail.com>
2024-10-04 12:04:08 +0800

    fix

--- lib/src/widget/heatmap_week_text.dart
+++ lib/src/widget/heatmap_week_text.dart
@@ -32,7 +32,7 @@ class HeatMapWeekText extends StatelessWidget {
     return Column(
       mainAxisSize: MainAxisSize.min,
       children: <Widget>[
-        for (String label in DateUtil.getShortWeekDays())
+        for (String label in weekDays)
           Container(
             height: size ?? 20,
             margin: margin ?? const EdgeInsets.all(2.0),
